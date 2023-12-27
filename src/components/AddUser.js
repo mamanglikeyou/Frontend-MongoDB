0b2ce223fe4403d8e6002a4c0f8856a01081e5f6
Dani Ramdani <danuramdanu7@gmail.com>
2023-12-27 20:39:00 +0700

    first commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const [nim, setNim] = useState("");
+  const [kelas, setKelas] = useState("");
+  const [major, setMajor] = useState("");
+  const [from, setFrom] = useState("");
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name,
+        email,
+        gender,
+        nim,
+        kelas,
+        major,
+        from,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="columns mt-5">
+      <div className="column is-half">
+        <form onSubmit={saveUser}>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Gender</label>
+            <div className="control">
+              <div className="select is-fullwidth">
+                <select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">NIM</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={nim}
+                onChange={(e) => setNim(e.target.value)}
+                placeholder="NIM"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Class</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={kelas}
+                onChange={(e) => setKelas(e.target.value)}
+                placeholder="Class"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Major</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={major}
+                onChange={(e) => setMajor(e.target.value)}
+                placeholder="Major"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">From</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={from}
+                onChange={(e) => setFrom(e.target.value)}
+                placeholder="From"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button type="submit" className="button is-success">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ src/components/EditUser.js
@@ -0,0 +1,152 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const [nim, setNim] = useState("");
+  const [kelas, setKelas] = useState("");
+  const [major, setMajor] = useState("");
+  const [from, setFrom] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
+  const getUserById = async () => {
+    const response = await axios.get(`http://localhost:5000/users/${id}`);
+    setName(response.data.name);
+    setEmail(response.data.email);
+    setGender(response.data.gender);
+    setNim(response.data.nim);
+    setKelas(response.data.kelas);
+    setMajor(response.data.major);
+    setFrom(response.data.from);
+  };
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http://localhost:5000/users/${id}`, {
+        name,
+        email,
+        gender,
+        nim,
+        kelas,
+        major,
+        from,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="columns mt-5">
+      <div className="column is-half">
+        <form onSubmit={updateUser}>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Gender</label>
+            <div className="control">
+              <div className="select is-fullwidth">
+                <select
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">NIM</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={nim}
+                onChange={(e) => setNim(e.target.value)}
+                placeholder="NIM"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Class</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={kelas}
+                onChange={(e) => setKelas(e.target.value)}
+                placeholder="Class"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Major</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={major}
+                onChange={(e) => setMajor(e.target.value)}
+                placeholder="Major"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">From</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={from}
+                onChange={(e) => setFrom(e.target.value)}
+                placeholder="From"
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button type="submit" className="button is-success">
+                Update
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
