bb6a53d785ec2bd034a791671633985cad2ce00f
Dani Ramdani <95326793+mamanglikeyou@users.noreply.github.com>
2023-12-28 20:45:39 +0700

    Update AddUser.js

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -15,7 +15,7 @@ const AddUser = () => {
   const saveUser = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/users", {
+      await axios.post("https://dani-backend-mongodb.vercel.app/users", {
         name,
         email,
         gender,
